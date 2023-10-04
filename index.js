827f2fff8b227cb6c3c7fa6efe26147c66319f6b
Huy <huy.ly@lansium.com>
2023-10-04 15:08:11 +0700

    update server loader

--- index.js
+++ index.js
@@ -9,18 +9,29 @@ import Services from './src/utils/services.js';
 import Queue from './src/utils/queue.js';
 import Schedule from './src/utils/schedule.js';
 
-const app = express();
-const server = http.createServer(app);
-server.listen(8000, (req, res) => {
-  Database.load();
-  Services.load();
-  Queue.load();
-  Schedule.load();
-  console.log('Server is started');
-});
+const load = async () => {
+  const app = express();
 
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-app.use([bodyParser.json(), bodyParser.urlencoded({ extended: false })]);
-app.use('/v1', router);
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use([bodyParser.json(), bodyParser.urlencoded({ extended: false })]);
+  app.use('/v1', router);
+
+  await Database.load();
+  await   Services.load();
+  await   Queue.load();
+  await Schedule.load();
+  const server = http.createServer(app);
+
+  server.listen(process.env.PORT || 8000);
+};
+
+load()
+  .then(() => {
+    console.log('Server started !');
+  })
+  .catch((error) => {
+    console.log('Server is crashed: ', error);
+    process.exit(1);
+  });
