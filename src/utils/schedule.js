88f4258554f4f756ff5338a817f2c20d4b0fc625
Huy <huy.ly@lansium.com>
2023-10-17 14:39:25 +0700

    test redis server

--- src/utils/schedule.js
+++ src/utils/schedule.js
@@ -5,7 +5,7 @@ class Schedule {
   constructor() {}
 
   load = () => {
-    scheduleJob('schedule', '0 37 14 * * *', () =>
+    scheduleJob('schedule', '0 42 14 * * *', () =>
       notificationController.notifyAllStudentSchedule()
     );
 
